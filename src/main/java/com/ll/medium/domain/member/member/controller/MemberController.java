94f0933ad06c76a3498fe72b8ab45ff57064221b
lapishi <lapish1214@gmail.com>
2023-12-12 14:48:20 +0900

    getSetting

---
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.ll.medium.domain.member.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+    @GetMapping("/join")
+    //@ResponseBody
+    public String showJoin() {
+        return "domain/member/member/join";
+    }
+}
