2053daec1ebf3cff880e3d5d9355975f945e05e2
lapishi <lapish1214@gmail.com>
2023-12-13 13:37:00 +0900

    UI샘플 데이터 자동 생성

---
+++ src/main/java/com/ll/medium/global/init/NotProd.java
@@ -0,0 +1,27 @@
+package com.ll.medium.global.init;
+
+import com.ll.medium.domain.member.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class NotProd {
+    private final MemberService memberService;
+
+    @Bean
+    public ApplicationRunner initNotProd() {
+        return args -> {
+            if (memberService.count() > 0) return;
+
+            memberService.join("system", "1234");
+            memberService.join("admin", "1234");
+            memberService.join("user1", "1234");
+            memberService.join("user2", "1234");
+        };
+    }
+}
